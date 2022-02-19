c10aa7031d7b1f6cef91221b93f2d5f17a62c595
luchx88 <lucianocavallo@gmail.com>
2022-02-19 04:11:22 -0300

    Styling MenuBar

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,13 +1,36 @@
 import logo from '../assets/logo.svg';
+import { FaHome, FaRegUser, FaFeather } from 'react-icons/fa';
 
 import '../styles/MenuBar.scss';
 
+const size = 35;
+
 export const MenuBar: React.FC = () => {
   return (
-    <div className="MenuBar">
-      <figure>
-        <img src={logo} />
-      </figure>
-    </div>
+    <ul className="MenuBar">
+      <li>
+        <button>
+          <img src={logo} alt="" />
+        </button>
+      </li>
+      <li>
+        <button>
+          <FaHome size={size} />
+          <span>Home</span>
+        </button>
+      </li>
+      <li>
+        <button>
+          <FaRegUser size={size} />
+          <span>My account</span>
+        </button>
+      </li>
+      <li>
+        <button className="MenuBar__NewPost">
+          <span>Post</span>
+          <FaFeather size={size} className="MenuBar__PostFeather" />
+        </button>
+      </li>
+    </ul>
   );
 };
