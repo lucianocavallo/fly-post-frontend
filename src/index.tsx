a2712f390077e406c05f2b5b395b055c9543fbcd
luchx88 <lucianocavallo@gmail.com>
2022-02-18 23:02:54 -0300

    Basic config Typescript, Webpack, Babel and Eslint.

---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+import reactDom from 'react-dom';
+import App from './App';
+
+reactDom.render(<App></App>, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg)$/,
+        type: 'asset/inline',
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+    }),
+  ],
+  devServer: {
+    open: true,
+    port: 3000,
+    historyApiFallback: true,
+  },
+};
